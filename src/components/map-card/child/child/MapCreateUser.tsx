efa20b7f554d4827e997039e22b921d57d0c1a03
Toshi7878 <toshiyuki19991125@gmail.com>
2024-12-29 02:09:22 +0900

    change: check-created-youtube-idのAPIをtRPCに移植

--- src/components/map-card/child/child/MapCreateUser.tsx
+++ src/components/map-card/child/child/MapCreateUser.tsx
@@ -1,17 +1,16 @@
-import { MapCardInfo } from "@/app/(home)/ts/type";
 import UpdateAtText from "@/components/custom-ui/UpdateAtText";
 import UserLinkText from "@/components/custom-ui/UserLinkText";
+import { RouterOutPuts } from "@/server/api/trpc";
 import { Box, Text } from "@chakra-ui/react";
-import React from "react";
 
 interface MapCreateUserProps {
-  map: MapCardInfo;
+  map: RouterOutPuts["map"]["getCreatedVideoIdMapList"][number];
 }
 
 const MapCreateUser = (props: MapCreateUserProps) => {
   return (
     <Text as="small" mt={2}>
-      <UserLinkText userId={props.map.user.id} userName={props.map.user.name} />
+      <UserLinkText userId={props.map.user.id} userName={props.map.user.name as string} />
       <Text as="span" fontSize="xs" display={{ base: "none", sm: "inline-block" }}>
         <Box mx={1}>
           - <UpdateAtText updatedAt={props.map.updatedAt} />
