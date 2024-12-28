312c112ff397204530dfab15b6ba6492c8473adc
Toshi7878 <toshiyuki19991125@gmail.com>
2024-12-28 23:54:26 +0900

    change: redirect無効

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -79,13 +79,13 @@ export const config: NextAuthConfig = {
       }
       return session;
     },
-    async redirect({ url, baseUrl }) {
-      // Allows relative callback URLs
-      if (url.startsWith("/")) return `${baseUrl}${url}`;
-      // Allows callback URLs on the same origin
-      if (new URL(url).origin === baseUrl) return url;
-      return baseUrl;
-    },
+    // redirect({ url, baseUrl }) {
+    //   // Allows relative callback URLs
+    //   if (url.startsWith("/")) return `${baseUrl}${url}`;
+    //   // Allows callback URLs on the same origin
+    //   if (new URL(url).origin === baseUrl) return url;
+    //   return baseUrl;
+    // },
   },
   pages: {
     signIn: "/",
