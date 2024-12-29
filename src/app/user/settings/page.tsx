f9856b8d78a0ca02ba8597e56cfca8d6b1cf0504
Toshi7878 <toshiyuki19991125@gmail.com>
2024-12-29 19:54:24 +0900

    add: user/settingsページ追加

---
+++ src/app/user/settings/page.tsx
@@ -0,0 +1,18 @@
+import { Box } from "@chakra-ui/react"; // Textを追加
+
+export default function Home() {
+  return (
+    <Box
+      as="main"
+      minH="100vw"
+      width={"100vw"}
+      display="flex"
+      flexDirection="column"
+      alignItems="baseline"
+      pt={20}
+      pl={"10%"}
+    >
+      {/* <Content /> */}
+    </Box>
+  );
+}
