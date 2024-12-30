61a4a4ca9c1de2d5e5147204321ad7ed5bfcef3a
Toshi7878 <toshiyuki19991125@gmail.com>
2024-12-30 20:36:39 +0900

    update

--- src/config/headerNav.ts
+++ src/config/headerNav.ts
@@ -1,6 +1,9 @@
 import { HeaderMenu } from "@/types";
 
-export const leftMenuItem: HeaderMenu[] = [{ title: "更新履歴", href: "/changelog" }];
+export const leftMenuItem: HeaderMenu[] = [
+  { title: "更新履歴", href: "/changelog" },
+  { title: "GitHub", href: "https://github.com/Toshi7878/YTyping" },
+];
 
 export const leftLink: HeaderMenu[] = [{ title: "タイムライン", href: "/timeline" }];
 
