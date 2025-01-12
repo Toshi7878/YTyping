08a2d489139182935907d26c07e5266d43b2f175
Toshi7878 <toshiyuki19991125@gmail.com>
2025-01-12 13:55:48 +0900

    update

--- src/app/edit/hooks/useChangeLineRowColor.ts
+++ src/app/edit/hooks/useChangeLineRowColor.ts
@@ -28,5 +28,33 @@ export const useChangeLineRowColor = () => {
 		tbodyRef.current!.children[seekCount].classList.add(LINE_ROW_SWITCH_CLASSNAMES.selected,LINE_ROW_SWITCH_CLASSNAMES.currentTime);
 	}
 
-  return {removeSelectedLineColor, removeCurrentTimeLineColor, addLineSeekColor}
+
+	const allUpdateSelectColor = (selectCount: number) => {
+		const tbodyChildren = tbodyRef.current?.children;
+		if (tbodyChildren) {
+		  for (let i = 0; i < tbodyChildren.length; i++) {
+			const trElement = tbodyChildren[i] as HTMLElement;
+			if (trElement.getAttribute("data-line-index") === String(selectCount)) {
+			  trElement.classList.add(LINE_ROW_SWITCH_CLASSNAMES.selected);
+			} else {
+			  trElement.classList.remove(LINE_ROW_SWITCH_CLASSNAMES.selected);
+			}
+		  }
+		}
+	}
+
+	const allUpdateCurrentTimeColor = (newCurrentCount: number) => {
+		const tbodyChildren = tbodyRef.current?.children;
+		if (tbodyChildren) {
+		  for (let i = 0; i < tbodyChildren.length; i++) {
+			const trElement = tbodyChildren[i] as HTMLElement;
+			if (trElement.getAttribute("data-line-index") === String(newCurrentCount)) {
+			  trElement.classList.add(LINE_ROW_SWITCH_CLASSNAMES.currentTime);
+			} else {
+			  trElement.classList.remove(LINE_ROW_SWITCH_CLASSNAMES.currentTime);
+			}
+		  }
+		}
+	}
+  return {removeSelectedLineColor, removeCurrentTimeLineColor, addLineSeekColor, allUpdateSelectColor, allUpdateCurrentTimeColor}
 }
--- src/app/edit/hooks/useUpdateCurrentLine.ts
+++ src/app/edit/hooks/useUpdateCurrentLine.ts
@@ -1,23 +1,12 @@
 import { useSetEditTimeCountAtom } from "../edit-atom/editAtom";
-import { useRefs } from "../edit-contexts/refsProvider";
-import { LINE_ROW_SWITCH_CLASSNAMES } from "../ts/const/editDefaultValues";
+import { useChangeLineRowColor } from "./useChangeLineRowColor";
 
 export const useUpdateCurrentLine = () => {
-  const { tbodyRef } = useRefs();
   const setTimeCount = useSetEditTimeCountAtom();
 
+  const { allUpdateCurrentTimeColor } = useChangeLineRowColor()
   return (newCount: number) => {
     setTimeCount(newCount);
-    const tbodyChildren = tbodyRef.current?.children;
-    if (tbodyChildren) {
-      for (let i = 0; i < tbodyChildren.length; i++) {
-        const trElement = tbodyChildren[i] as HTMLElement;
-        if (trElement.getAttribute("data-line-index") === String(newCount)) {
-          trElement.classList.add(LINE_ROW_SWITCH_CLASSNAMES.currentTime);
-        } else {
-          trElement.classList.remove(LINE_ROW_SWITCH_CLASSNAMES.currentTime);
-        }
-      }
-    }
+    allUpdateCurrentTimeColor(newCount)
   };
 };
