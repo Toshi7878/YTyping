73aae7edf12eaedeb87251b50ba331af08f94878
Toshi7878 <toshiyuki19991125@gmail.com>
2025-01-13 19:08:38 +0900

    update

--- src/app/type/_components/type-tab-content/child/icon-child/EditIcon.tsx
+++ src/app/type/_components/type-tab-content/child/icon-child/EditIcon.tsx
@@ -3,6 +3,7 @@ import { useLinkClick } from "@/lib/global-hooks/useLinkClick";
 import { ThemeColors } from "@/types";
 import { Link } from "@chakra-ui/next-js";
 import { Box, useBreakpointValue, useTheme } from "@chakra-ui/react";
+import { useSession } from "next-auth/react";
 import { useParams } from "next/navigation";
 import { BiEdit } from "react-icons/bi";
 
@@ -11,9 +12,11 @@ const EditIcon = () => {
   const { id: mapId } = useParams();
   const handleLinkClick = useLinkClick();
   const iconSize = useBreakpointValue({ base: 72, md: 36 });
+  const { data: session } = useSession();
 
+  const tooltipLabel = `譜面のEditページに移動${session?.user.role === "USER" ? "(閲覧のみ)" : ""}`;
   return (
-    <CustomToolTip label="譜面のEditページに移動" placement="top">
+    <CustomToolTip label={tooltipLabel} placement="top">
       <Box height="60px" display="flex" alignItems="center">
         <Link
           href={`/edit/${mapId}`}
