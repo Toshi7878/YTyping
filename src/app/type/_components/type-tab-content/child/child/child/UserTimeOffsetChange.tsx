438958ecf019043ae24fa44cdc591b3376dd81ee
Toshi7878 <toshiyuki19991125@gmail.com>
2025-02-02 01:48:09 +0900

    update

--- src/app/type/_components/type-tab-content/child/child/child/UserTimeOffsetChange.tsx
+++ src/app/type/_components/type-tab-content/child/child/child/UserTimeOffsetChange.tsx
@@ -6,6 +6,7 @@ import {
   useSetUserOptionsAtom,
   useUserOptionsAtom,
 } from "@/app/type/type-atoms/gameRenderAtoms";
+import CustomToolTip from "@/components/custom-ui/CustomToolTip";
 import { ThemeColors } from "@/types";
 import { Box, Button, Flex, Text, useTheme } from "@chakra-ui/react";
 
@@ -45,13 +46,17 @@ const UserTimeOffsetChange = () => {
         width="fit-content"
         rounded={"full"}
       >
-        <Button onClick={decrement} cursor="pointer" variant="unstyled" size="lg" fontSize="xl">
-          -
-        </Button>
+        <CustomToolTip label={"タイミングが早くなります"} placement="top">
+          <Button onClick={decrement} cursor="pointer" variant="unstyled" size="lg" fontSize="xl">
+            -
+          </Button>
+        </CustomToolTip>
         <Box fontSize="lg">{userOptionsAtom.time_offset.toFixed(2)}</Box>
-        <Button onClick={increment} cursor="pointer" variant="unstyled" size="lg" fontSize="xl">
-          +
-        </Button>
+        <CustomToolTip label={"タイミングが遅くなります"} placement="top">
+          <Button onClick={increment} cursor="pointer" variant="unstyled" size="lg" fontSize="xl">
+            +
+          </Button>
+        </CustomToolTip>
       </Flex>
     </Flex>
   );
