4f1e860fe02ea2d46e70ff2aec071ba5389ea107
Toshi7878 <toshiyuki19991125@gmail.com>
2025-02-09 19:03:57 +0900

    update

--- src/components/share-components/text/ResultToolTipText.tsx
+++ src/components/share-components/text/ResultToolTipText.tsx
@@ -1,5 +1,5 @@
 import { ThemeColors } from "@/types";
-import { Box, Text, useTheme } from "@chakra-ui/react";
+import { Box, Divider, HStack, Text, useTheme, VStack } from "@chakra-ui/react";
 
 interface ResultToolTipTextProps {
   romaType: number;
@@ -26,74 +26,84 @@ const ResultToolTipText = (props: ResultToolTipTextProps) => {
   const theme: ThemeColors = useTheme();
 
   return (
-    <Box fontSize="sm" lineHeight={7}>
-      <TypeCountResult
-        romaType={props.romaType}
-        kanaType={props.kanaType}
-        flickType={props.flickType}
-        englishType={props.englishType}
-        numType={props.numType}
-        symbolType={props.symbolType}
-        spaceType={props.spaceType}
-      />
-      <Box>
-        ミス数:{" "}
-        <Text as="span" fontSize="md" fontWeight="bold">
-          {props.miss} ({props.correctRate}%)
-        </Text>
-      </Box>
-      <Box>
-        ロスト数:{" "}
-        <Text as="span" fontSize="md" fontWeight="bold">
-          {props.lost}
-        </Text>
-      </Box>
-      <Box>
-        最大コンボ:{" "}
-        <Text
-          as="span"
-          {...(props.isPerfect && { color: theme.colors.semantic.perfect })}
-          className={`${props.isPerfect ? "outline-text" : ""}`}
-          fontSize="md"
-          fontWeight="bold"
-        >
-          {props.maxCombo}
-        </Text>
-      </Box>
-      <Box>
-        rkpm:{" "}
-        <Text as="span" fontSize="md" fontWeight="bold">
-          {props.rkpm}
-        </Text>
-      </Box>
-      {props.isKanaFlickTyped && props.kpm !== props.romaKpm && (
-        <Box>
-          <Text as="span">ローマ字換算kpm</Text>:{" "}
-          <Text as="span" fontSize="md" fontWeight="bold">
-            {props.romaKpm}
+    <Box p={4} borderRadius="md" boxShadow="md">
+      <VStack spacing={3} align="start">
+        <TypeCountResult
+          romaType={props.romaType}
+          kanaType={props.kanaType}
+          flickType={props.flickType}
+          englishType={props.englishType}
+          numType={props.numType}
+          symbolType={props.symbolType}
+          spaceType={props.spaceType}
+        />
+        <Divider />
+
+        <HStack spacing={2}>
+          <Text>ミス数:</Text>
+          <Text fontSize="md" fontWeight="bold">
+            {props.miss} ({props.correctRate}%)
           </Text>
-        </Box>
-      )}
-      {props.defaultSpeed > 1 && (
-        <Box>
-          倍速:{" "}
-          <Text as="span" fontSize="md" fontWeight="bold">
-            {props.defaultSpeed.toFixed(2)}x
+        </HStack>
+
+        <HStack spacing={2}>
+          <Text>ロスト数:</Text>
+          <Text fontSize="md" fontWeight="bold">
+            {props.lost}
           </Text>
-        </Box>
-      )}
-      <Box>
-        日時:{" "}
-        <Text as="span" fontSize="md">
-          {new Date(props.updatedAt).toLocaleString("ja-JP", {
-            year: "numeric",
-            month: "2-digit",
-            day: "2-digit",
-            hour: "2-digit",
-            minute: "2-digit",
-          })}
-        </Text>
-      </Box>
+        </HStack>
+
+        <HStack spacing={2}>
+          <Text>最大コンボ:</Text>
+          <Text
+            as="span"
+            fontSize="md"
+            fontWeight="bold"
+            color={props.isPerfect ? theme.colors.semantic.perfect : undefined}
+            className={props.isPerfect ? "outline-text" : undefined}
+          >
+            {props.maxCombo}
+          </Text>
+        </HStack>
+
+        <HStack spacing={2}>
+          <Text>rkpm:</Text>
+          <Text fontSize="md" fontWeight="bold">
+            {props.rkpm}
+          </Text>
+        </HStack>
+
+        {props.isKanaFlickTyped && props.kpm !== props.romaKpm && (
+          <HStack spacing={2}>
+            <Text>ローマ字換算kpm:</Text>
+            <Text fontSize="md" fontWeight="bold">
+              {props.romaKpm}
+            </Text>
+          </HStack>
+        )}
+
+        {props.defaultSpeed > 1 && (
+          <HStack spacing={2}>
+            <Text>倍速:</Text>
+            <Text fontSize="md" fontWeight="bold">
+              {props.defaultSpeed.toFixed(2)}x
+            </Text>
+          </HStack>
+        )}
+
+        <HStack spacing={2}>
+          <Text>日時:</Text>
+          <Text fontSize="md">
+            {new Date(props.updatedAt).toLocaleString("ja-JP", {
+              year: "numeric",
+              month: "2-digit",
+              day: "2-digit",
+              hour: "2-digit",
+              minute: "2-digit",
+            })}
+          </Text>
+        </HStack>
+      </VStack>
     </Box>
   );
 };
@@ -109,7 +119,6 @@ interface TypeCountResultProps {
 }
 
 const TypeCountResult = (props: TypeCountResultProps) => {
-  // 各入力タイプの情報を配列にまとめる
   const types = [
     { label: "ローマ字タイプ数", value: props.romaType },
     { label: "かな入力タイプ数", value: props.kanaType },
@@ -123,27 +132,28 @@ const TypeCountResult = (props: TypeCountResultProps) => {
   const typesUsedCount = types.filter((type) => type.value > 0).length;
 
   return (
-    <>
+    <VStack spacing={1} align="start">
       {types.map(
         (type, index) =>
           type.value > 0 && (
-            <Box key={index}>
-              <Text as="span">{type.label}</Text>:{" "}
-              <Text as="span" fontSize="md" fontWeight="bold">
+            <HStack key={index} spacing={2}>
+              <Text>{type.label}:</Text>
+              <Text fontSize="md" fontWeight="bold">
                 {type.value}
               </Text>
-            </Box>
+            </HStack>
           )
       )}
       {typesUsedCount > 1 && (
-        <Box>
-          <Text as="span">合計タイプ数</Text>:{" "}
-          <Text as="span" fontSize="md" fontWeight="bold">
+        <HStack spacing={2}>
+          <Text>合計タイプ数:</Text>
+          <Text fontSize="md" fontWeight="bold">
             {total}
           </Text>
-        </Box>
+        </HStack>
       )}
-    </>
+    </VStack>
   );
 };
+
 export default ResultToolTipText;
