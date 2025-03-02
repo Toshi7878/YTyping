8f462280a32d0ae109d444922ef17e771fdb4859
Toshi7878 <toshiyuki19991125@gmail.com>
2025-03-02 13:44:31 +0900

    update

--- src/app/(home)/components/search/SearchContent.tsx
+++ src/app/(home)/components/search/SearchContent.tsx
@@ -2,11 +2,13 @@ import { useGlobalRefs } from "@/app/_components/global-provider/GlobalRefProvid
 import VolumeRange from "@/components/share-components/VolumeRange";
 import { IS_ANDROID, IS_IOS } from "@/config/consts/globalConst";
 import { Box, Flex } from "@chakra-ui/react";
+import { useSession } from "next-auth/react";
 import FilterInputs from "./child/FilterInputs";
 import SearchInputs from "./child/SearchInputs";
 
 const SearchContent = () => {
   const { playerRef } = useGlobalRefs();
+  const { data: session } = useSession();
 
   return (
     <Flex as="section" width="100%" alignItems="center" mb={4}>
@@ -14,13 +16,9 @@ const SearchContent = () => {
         <Box mb={3}>
           <SearchInputs />
         </Box>
-        <Flex justifyContent="space-between">
-          <FilterInputs />
-          {!IS_IOS && !IS_ANDROID && (
-            <Flex justifyContent="flex-end">
-              <VolumeRange playerRef={playerRef} />
-            </Flex>
-          )}
+        <Flex justifyContent={session?.user?.id ? "space-between" : "flex-end"}>
+          {session?.user?.id && <FilterInputs />}
+          {!IS_IOS && !IS_ANDROID && <VolumeRange playerRef={playerRef} />}
         </Flex>
       </Box>
     </Flex>
