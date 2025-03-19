56b54b1fdb8a13e5ec9903e4b9d4d4825a1ab701
Toshi7878 <toshiyuki19991125@gmail.com>
2025-03-19 23:00:27 +0900

    update

--- src/server/api/routers/userTypingOptionRouter.ts
+++ src/server/api/routers/userTypingOptionRouter.ts
@@ -2,6 +2,19 @@ import { line_completed_display, next_display, time_offset_key, toggle_input_mod
 import { z } from "zod";
 import { protectedProcedure, publicProcedure } from "../trpc";
 
+const typingOptionSchema = z.object({
+  time_offset: z.number(),
+  kana_word_scroll: z.number(),
+  roma_word_scroll: z.number(),
+  type_sound: z.boolean(),
+  miss_sound: z.boolean(),
+  line_clear_sound: z.boolean(),
+  line_completed_display: z.nativeEnum(line_completed_display),
+  next_display: z.nativeEnum(next_display),
+  time_offset_key: z.nativeEnum(time_offset_key),
+  toggle_input_mode_key: z.nativeEnum(toggle_input_mode_key),
+});
+
 export const userTypingOptionRouter = {
   getUserTypingOptions: publicProcedure.query(async ({ ctx }) => {
     const { db, user } = ctx;
@@ -24,37 +37,22 @@ export const userTypingOptionRouter = {
 
     return userTypingOptions;
   }),
-  update: protectedProcedure
-    .input(
-      z.object({
-        time_offset: z.number(),
-        kana_word_scroll: z.number(),
-        roma_word_scroll: z.number(),
-        type_sound: z.boolean(),
-        miss_sound: z.boolean(),
-        line_clear_sound: z.boolean(),
-        line_completed_display: z.nativeEnum(line_completed_display),
-        next_display: z.nativeEnum(next_display),
-        time_offset_key: z.nativeEnum(time_offset_key),
-        toggle_input_mode_key: z.nativeEnum(toggle_input_mode_key),
-      })
-    )
-    .mutation(async ({ input, ctx }) => {
-      const { db, user } = ctx;
+  update: protectedProcedure.input(typingOptionSchema).mutation(async ({ input, ctx }) => {
+    const { db, user } = ctx;
 
-      const updated = await db.user_typing_options.upsert({
-        where: {
-          user_id: user.id,
-        },
-        update: {
-          ...input,
-        },
-        create: {
-          user_id: user.id,
-          ...input,
-        },
-      });
+    const updated = await db.user_typing_options.upsert({
+      where: {
+        user_id: user.id,
+      },
+      update: {
+        ...input,
+      },
+      create: {
+        user_id: user.id,
+        ...input,
+      },
+    });
 
-      return updated;
-    }),
+    return updated;
+  }),
 };
