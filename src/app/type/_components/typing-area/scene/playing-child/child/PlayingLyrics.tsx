1286349f2c7ff7a825307891f79efc80bd5b3e66
Toshi7878 <toshiyuki19991125@gmail.com>
2025-03-29 00:21:22 +0900

    update

--- src/app/type/_components/typing-area/scene/playing-child/child/PlayingLyrics.tsx
+++ src/app/type/_components/typing-area/scene/playing-child/child/PlayingLyrics.tsx
@@ -15,13 +15,26 @@ const PlayingLyrics = () => {
       width="103%"
       className={"lyrics-font"}
       whiteSpace="nowrap"
-      {...(userOptionsAtom.line_completed_display === "NEXT_WORD" && {
-        sx: {
+      sx={{
+        ruby: {
+          display: "inline-flex",
+          flexDirection: "column",
+          alignItems: "center",
+          textAlign: "center",
+          lineHeight: "normal",
+        },
+        rt: {
+          display: "block",
+          textAlign: "center",
+          fontSize: "0.5em",
+          lineHeight: "1",
+        },
+        ...(userOptionsAtom.line_completed_display === "NEXT_WORD" && {
           ".word-area-completed + &": {
             visibility: "hidden",
           },
-        },
-      })}
+        }),
+      }}
     >
       {parse(`${lyrics}<ruby class="invisible">あ<rt>あ</rt></ruby>`)}
     </Box>
