99a2aa3e6974be7b88e3bb8821030f086d51ba9b
Toshi7878 <toshiyuki19991125@gmail.com>
2025-03-29 19:20:30 +0900

    update

--- src/app/type/_components/typing-area/scene/child/top-child/PlayingNotify.tsx
+++ src/app/type/_components/typing-area/scene/child/top-child/PlayingNotify.tsx
@@ -5,13 +5,9 @@ import { AnimatePresence, motion } from "framer-motion"; // 追加
 import { useEffect, useRef } from "react";
 import { FaPause, FaPlay } from "react-icons/fa6";
 
-interface PlayingNotifyProps {
-  className?: string;
-}
-
 const NON_ANIMATED = ["ll", "Replay", "Practice"];
 
-const PlayingNotify = ({ className = "" }: PlayingNotifyProps) => {
+const PlayingNotify = () => {
   const notify = useNotifyState();
   const setNotify = useSetNotifyState();
   const scene = useSceneState();
@@ -29,8 +25,7 @@ const PlayingNotify = ({ className = "" }: PlayingNotifyProps) => {
   };
 
   useEffect(() => {
-    if (notify.description && !NON_ANIMATED.includes(notify.description)) {
-      // 1秒後にhandleExitCompleteを強制的に実行
+    if (!NON_ANIMATED.includes(notify.description || "")) {
       timerRef.current = setTimeout(() => {
         handleExitComplete();
       }, 800);
@@ -45,11 +40,11 @@ const PlayingNotify = ({ className = "" }: PlayingNotifyProps) => {
   }, [notify.description]);
 
   const handleExitComplete = () => {
-    // exitアニメーション完了時の処理をここに記述
     if (timerRef.current) {
       clearTimeout(timerRef.current);
     }
-    if (!NON_ANIMATED.includes(notify.description!)) {
+
+    if (!NON_ANIMATED.includes(notify.description || "")) {
       playModeNotify();
     }
   };
@@ -60,22 +55,11 @@ const PlayingNotify = ({ className = "" }: PlayingNotifyProps) => {
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [scene]);
+
   return (
-    <Box
-      position="absolute"
-      left="41%"
-      whiteSpace="nowrap"
-      textAlign="center"
-      className={`${className}`}
-      userSelect="none"
-      id="playing_notify"
-    >
-      {notify.description && NON_ANIMATED.includes(notify.description) ? (
-        <Box
-          className={`${className} ${
-            notify.description === "Replay" || notify.description === "Practice" ? "opacity-30" : ""
-          }`}
-        >
+    <Box position="absolute" left="41%" whiteSpace="nowrap" textAlign="center" userSelect="none" id="playing_notify">
+      {NON_ANIMATED.includes(notify.description || "") ? (
+        <Box className={notify.description === "Replay" || notify.description === "Practice" ? "opacity-30" : ""}>
           {notify.description === "ll" ? (
             <FaPause />
           ) : notify.description === "Replay" ? (
@@ -86,17 +70,15 @@ const PlayingNotify = ({ className = "" }: PlayingNotifyProps) => {
         </Box>
       ) : (
         <AnimatePresence mode="popLayout" onExitComplete={handleExitComplete}>
-          {notify.description && (
-            <motion.div
-              key={Date.now()}
-              initial={{ opacity: 1 }}
-              animate={{ opacity: 0 }}
-              transition={{ duration: 0.5 }}
-              exit={{ opacity: 0 }}
-            >
-              <Box>{notify.description === "▶" ? <FaPlay /> : notify.description}</Box>
-            </motion.div>
-          )}
+          <motion.div
+            key={Date.now()}
+            initial={{ opacity: 1 }}
+            animate={{ opacity: 0 }}
+            transition={{ duration: 0.5 }}
+            exit={{ opacity: 0 }}
+          >
+            <Box>{notify.description === "▶" ? <FaPlay /> : notify.description}</Box>
+          </motion.div>
         </AnimatePresence>
       )}
     </Box>
