ef988af0251a97bd372ccf3854bf804c53593321
Toshi7878 <toshiyuki19991125@gmail.com>
2025-03-30 15:11:43 +0900

    update

--- src/app/(menus)/credit/Content.tsx
+++ src/app/(menus)/credit/Content.tsx
@@ -1,27 +1,9 @@
 "use client";
 
-import { Image } from "@chakra-ui/next-js";
+import CustomCard from "@/components/custom-ui/CustomCard";
 import { Box, CardBody, Divider, Heading, Link, ListItem, OrderedList, Text } from "@chakra-ui/react";
 
-function detectBrowserType() {
-  if (typeof window === "undefined") return "chrome";
-
-  const userAgent = window.navigator.userAgent.toLowerCase();
-
-  if (userAgent.indexOf("firefox") > -1) {
-    return "firefox";
-  } else if (userAgent.indexOf("edg") > -1) {
-    return "edge";
-  } else if (userAgent.indexOf("safari") > -1 && userAgent.indexOf("chrome") === -1) {
-    return "safari";
-  } else {
-    return "chrome";
-  }
-}
-
 export default function PreMidManual() {
-  const browserType = detectBrowserType();
-
   return (
     <Box mx="auto" py={8} px={4}>
       <Heading as="h1" size="xl" mb={8}>
@@ -39,12 +21,7 @@ export default function PreMidManual() {
           <OrderedList spacing={6}>
             <ListItem>
               <Text fontWeight="bold">PreMiDブラウザ拡張機能をインストールする</Text>
-              <Text mt={2}>
-                <Link href={browserLinks[browserType].url} color="primary.main" isExternal>
-                  {browserLinks[browserType].text}
-                </Link>
-                から拡張機能をインストールします。
-              </Text>
+              <Text mt={2}></Text>
             </ListItem>
 
             <Divider my={4} />
@@ -67,14 +44,6 @@ export default function PreMidManual() {
                 <Text>
                   PreMiD拡張機能を初めて開くと、以下の表示が出てくるので、表示したいDiscordアカウントとリンクする
                 </Text>
-                <Image
-                  alt="PreMID拡張機能を開いてDiscordアカウントとリンクします。"
-                  src={preMidLinks}
-                  width="250"
-                  border={"solid 1px"}
-                  borderColor={"border.card"}
-                  mt={2}
-                />
               </Box>
             </ListItem>
 
@@ -83,22 +52,7 @@ export default function PreMidManual() {
             <ListItem>
               <Text fontWeight="bold">YTypingをプレイする</Text>
               <Text mt={2}>YTypingをプレイすると、自動的にDiscordのステータスに表示されます。</Text>
-              <Box mt={2} display="flex" flexDirection="column" gap={4}>
-                <Image
-                  width="250"
-                  alt="スクリーンショット1"
-                  src={preMidPresence1}
-                  border={"solid 1px"}
-                  borderColor={"border.card"}
-                />
-                <Image
-                  width="250"
-                  alt="スクリーンショット2"
-                  src={preMidPresence2}
-                  border={"solid 1px"}
-                  borderColor={"border.card"}
-                />
-              </Box>
+              <Box mt={2} display="flex" flexDirection="column" gap={4}></Box>
             </ListItem>
           </OrderedList>
         </CardBody>
