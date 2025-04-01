e572b20fdeea71484ff947e466a897bc89de9a82
Toshi7878 <toshiyuki19991125@gmail.com>
2025-04-01 23:16:01 +0900

    update

--- src/components/map-card/child/child/MapBadgesLayout.tsx
+++ src/components/map-card/child/child/MapBadgesLayout.tsx
@@ -1,6 +1,7 @@
 import LikeCountIcon from "@/components/share-components/map-icons/LikeCountIcon";
 import RankingCountIcon from "@/components/share-components/map-icons/RankingCountIcon";
 import { RouterOutPuts } from "@/server/api/trpc";
+import { formatTime } from "@/util/formatTime";
 import { Flex, HStack, Text } from "@chakra-ui/react";
 import MapBadge from "./MapBadge";
 
@@ -12,11 +13,7 @@ const MapBadges = (props: MapBadgesProps) => {
   const { map } = props;
 
   return (
-    <Flex
-      justifyContent={{ base: "flex-end", md: "space-between" }}
-      width={{ base: "fit-content", lg: "98%" }}
-      mr={3}
-    >
+    <Flex justifyContent={{ base: "flex-end", md: "space-between" }} width={{ base: "fit-content", lg: "98%" }} mr={3}>
       <HStack mr={2}>
         <MapBadge>
           <Text as="span" fontSize="xs" display={{ base: "none", sm: "inline-block" }}>
@@ -24,24 +21,11 @@ const MapBadges = (props: MapBadgesProps) => {
           </Text>
           {(map.difficulty!.roma_kpm_median / 100).toFixed(1)}
         </MapBadge>
-        <MapBadge display={{ base: "none", md: "block" }}>
-          {new Date(map.difficulty!.total_time * 1000)
-            .toISOString()
-            .slice(11, 19)
-            .replace(/^00:/, "")}
-        </MapBadge>
+        <MapBadge display={{ base: "none", md: "block" }}>{formatTime(map.difficulty!.total_time)}</MapBadge>
       </HStack>
       <Flex>
-        <RankingCountIcon
-          key={map.results[0]?.rank}
-          myRank={map.results[0]?.rank}
-          rankingCount={map.ranking_count}
-        />
-        <LikeCountIcon
-          mapId={map.id}
-          isLiked={!!map.map_likes[0]?.is_liked}
-          likeCount={map.like_count}
-        />
+        <RankingCountIcon key={map.results[0]?.rank} myRank={map.results[0]?.rank} rankingCount={map.ranking_count} />
+        <LikeCountIcon mapId={map.id} isLiked={!!map.map_likes[0]?.is_liked} likeCount={map.like_count} />
       </Flex>
     </Flex>
   );
