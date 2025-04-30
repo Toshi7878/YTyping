aa4bbfdc4e952af4bb8b041f550a41e0bee54d65
Toshi7878 <toshiyuki19991125@gmail.com>
2025-04-30 22:59:46 +0900

    update

--- src/util/global-hooks/query/useMapListQuery.ts
+++ src/util/global-hooks/query/useMapListQuery.ts
@@ -60,9 +60,6 @@ export const useMapListInfiniteQuery = () => {
     initialPageParam: 0,
     getNextPageParam: (lastPage, allPages) => (lastPage.maps.length === PAGE_SIZE ? allPages.length : undefined),
     getPreviousPageParam: (firstPage, allPages) => (allPages.length > 1 ? allPages.length - 2 : undefined),
-    refetchOnWindowFocus: false,
-    refetchOnReconnect: false,
-    refetchOnMount: true,
     gcTime: Infinity,
     staleTime: params.sort?.includes("random") ? Infinity : 1000 * 20,
   });
