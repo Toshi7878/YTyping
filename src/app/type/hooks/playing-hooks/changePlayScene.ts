caeddb87fa4bd0b6d47978230bf999d8dd09284b
Toshi7878 <toshiyuki19991125@gmail.com>
2025-05-11 15:52:29 +0900

    update

--- src/app/type/hooks/playing-hooks/changePlayScene.ts
+++ src/app/type/hooks/playing-hooks/changePlayScene.ts
@@ -1,7 +1,8 @@
 import { YouTubeSpeed } from "@/types";
 import { useGameUtilityReferenceParams, useLineStatus } from "../../atoms/refAtoms";
 import { usePlaySpeedReducer } from "../../atoms/speedReducerAtoms";
-import { useReadGameUtilParams, useSetNotify, useSetScene } from "../../atoms/stateAtoms";
+import { useReadGameUtilParams, useSetNotify, useSetPlayingInputMode, useSetScene } from "../../atoms/stateAtoms";
+import { useReadReadyInputMode } from "../../atoms/storageAtoms";
 import { useRetry } from "./retry";
 
 export const useChangePlayMode = () => {
@@ -12,10 +13,13 @@ export const useChangePlayMode = () => {
 
   const { writeGameUtilRefParams, readGameUtilRefParams } = useGameUtilityReferenceParams();
   const { readLineStatus } = useLineStatus();
-  const readGameStateUtils = useReadGameUtilParams();
+  const readGameUtilParams = useReadGameUtilParams();
+  const setPlayingInputMode = useSetPlayingInputMode();
+
+  const readReadyInputMode = useReadReadyInputMode();
 
   return () => {
-    const { scene } = readGameStateUtils();
+    const { scene } = readGameUtilParams();
     if (scene === "play") {
       const confirmMessage = "練習モードに移動しますか？";
       if (window.confirm(confirmMessage)) {
@@ -25,11 +29,16 @@ export const useChangePlayMode = () => {
       const confirmMessage = "本番モードに移動しますか？了承すると初めから再生されます。";
       if (window.confirm(confirmMessage)) {
         const { startSpeed } = readLineStatus();
+
         writeGameUtilRefParams({ practiceMyResultId: null, replayKeyCount: 0, replayUserName: "" });
         const { lineResultdrawerClosure: drawerClosure } = readGameUtilRefParams();
 
         if (drawerClosure) {
           drawerClosure.onClose();
+        }
+
+        if (scene === "replay") {
+          setPlayingInputMode(readReadyInputMode());
         }
         retry("play");
         dispatchSpeed({ type: "set", payload: 1 > startSpeed ? 1 : (startSpeed as YouTubeSpeed) });
