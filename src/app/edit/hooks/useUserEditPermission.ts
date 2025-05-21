bb044e1dd8430801b8c97b4eeacafdd30ee8da94
Toshi7878 <toshiyuki19991125@gmail.com>
2025-05-21 23:45:17 +0900

    update

--- src/app/edit/hooks/useUserEditPermission.ts
+++ src/app/edit/hooks/useUserEditPermission.ts
@@ -1,14 +1,18 @@
 import { useSession } from "next-auth/react";
+import { useSearchParams } from "next/navigation";
 import { useMapCreatorIdState } from "../atoms/stateAtoms";
 
 const useHasEditPermission = () => {
   const { data: session } = useSession();
   const mapCreatorId = useMapCreatorIdState();
+  const searchParams = useSearchParams();
+
+  const isNewCreate = !!searchParams.get("new");
 
   const myUserId = session?.user?.id;
   const isAdmin = session?.user?.role === "ADMIN";
 
-  return (myUserId && (!mapCreatorId || Number(myUserId) === mapCreatorId)) || isAdmin;
+  return isNewCreate || (myUserId && (!mapCreatorId || Number(myUserId) === mapCreatorId)) || isAdmin;
 };
 
 export default useHasEditPermission;
