3e384eaa4a87ebfc1f0c97afa229d281ca52e4f0
Toshi7878 <toshiyuki19991125@gmail.com>
2025-05-31 21:45:27 +0900

    update

--- src/app/user/[id]/page.tsx
+++ src/app/user/[id]/page.tsx
@@ -1,10 +1,11 @@
 import { serverApi } from "@/trpc/server";
 import Content from "./Content";
 
-export default async function Home({ params }: { params: { id: string } }) {
-  const userProfile = await serverApi.user.getUserProfile({ userId: Number(params.id) });
-  const userStats = await serverApi.userStats.getUserStats({ userId: Number(params.id) });
-  const userOptions = await serverApi.userOption.getUserOptions({ userId: Number(params.id) });
+export default async function Home({ params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params;
+  const userProfile = await serverApi.user.getUserProfile({ userId: Number(id) });
+  const userStats = await serverApi.userStats.getUserStats({ userId: Number(id) });
+  const userOptions = await serverApi.userOption.getUserOptions({ userId: Number(id) });
 
   return <Content userProfile={userProfile} userStats={userStats} userOptions={userOptions} />;
 }
