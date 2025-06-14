c9f6e9590ebc63fcc21d6236ff18b132f24297c9
Toshi7878 <toshiyuki19991125@gmail.com>
2025-06-14 18:51:00 +0900

    update

--- src/components/ui/input/input.tsx
+++ src/components/ui/input/input.tsx
@@ -2,12 +2,11 @@ import { cn } from "@/lib/utils";
 import { cva, type VariantProps } from "class-variance-authority";
 import * as React from "react";
 
-// TODO:chakra ui 移行後 !important削除
 const inputVariants = cva(
   [
     "placeholder:text-muted-foreground selection:bg-primary",
     "selection:text-primary-foreground dark:bg-input/30 border-input flex h-9 w-full min-w-0",
-    "rounded-sm !border !border-solid bg-transparent !px-3 py-2 text-base shadow-xs transition-[color,box-shadow] outline-none",
+    "rounded-sm border border-solid bg-transparent px-3 py-2 text-base shadow-xs transition-[color,box-shadow] outline-none",
     "file:text-foreground file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium",
     "disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50",
     "md:text-sm focus-visible:border-ring focus-visible:ring-ring focus-visible:ring-[2px]",
@@ -15,7 +14,7 @@ const inputVariants = cva(
   {
     variants: {
       variant: {
-        default: "!border-border/50",
+        default: "border-border/50",
         error: "border-destructive aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40",
         success: "border-green-500 focus-visible:border-green-600 focus-visible:ring-green-500/50",
         warning: "border-yellow-500 focus-visible:border-yellow-600 focus-visible:ring-yellow-500/50",
