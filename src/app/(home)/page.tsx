89d66a42730de7d378930d818b5fbd98834dd109
Toshi7878 <toshiyuki19991125@gmail.com>
2025-08-24 10:52:46 +0900

    update

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,17 +1,14 @@
-import { HydrateClient } from "@/trpc/server";
 import MapControlArea from "./_components/MapControlArea";
 import MapList from "./_components/MapList";
 import HomeProvider from "./HomeClientProvider";
 
 export default function Home() {
   return (
-    <HydrateClient>
-      <HomeProvider>
-        <div className="mx-auto max-w-screen-xl lg:px-8">
-          <MapControlArea />
-          <MapList />
-        </div>
-      </HomeProvider>
-    </HydrateClient>
+    <HomeProvider>
+      <div className="mx-auto max-w-screen-xl lg:px-8">
+        <MapControlArea />
+        <MapList />
+      </div>
+    </HomeProvider>
   );
 }
