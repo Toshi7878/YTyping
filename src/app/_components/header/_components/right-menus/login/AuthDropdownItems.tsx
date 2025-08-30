c794ad7d143ac6872bbe748110a50fd33935bdb9
Toshi7878 <toshiyuki19991125@gmail.com>
2025-08-30 21:21:38 +0900

    update

--- src/app/_components/header/_components/right-menus/login/AuthDropdownItems.tsx
+++ src/app/_components/header/_components/right-menus/login/AuthDropdownItems.tsx
@@ -11,13 +11,13 @@ export const SignInDropdownItems = () => {
       text: "Discordでログイン",
       leftIcon: <BsDiscord className="size-6 text-white" />,
       provider: "discord",
-      hover: "focus:bg-discord",
+      className: "hover:bg-discord focus:bg-discord",
     },
     {
       text: "Googleでログイン",
       leftIcon: <BsGoogle className="size-6 text-white" />,
       provider: "google",
-      hover: "focus:bg-google",
+      className: "hover:bg-google focus:bg-google",
     },
   ];
 
@@ -30,7 +30,7 @@ export const SignInDropdownItems = () => {
             start();
             await signIn(item.provider);
           }}
-          className={cn("flex w-full items-center gap-3 p-3 px-6 text-white", item.hover)}
+          className={cn("flex w-full items-center gap-3 p-3 px-6 text-white", item.className)}
         >
           {item.leftIcon}
           <span className="font-semibold">{item.text}</span>
