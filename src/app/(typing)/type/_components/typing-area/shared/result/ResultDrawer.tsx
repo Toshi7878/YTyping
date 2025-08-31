c7cf9c0289afbdaabd72eed2c55d3450e9acf732
Toshi7878 <toshiyuki19991125@gmail.com>
2025-08-31 17:56:17 +0900

    update

--- src/app/(typing)/type/_components/typing-area/shared/result/ResultDrawer.tsx
+++ src/app/(typing)/type/_components/typing-area/shared/result/ResultDrawer.tsx
@@ -1,14 +1,19 @@
 "use client";
-import { useLineResultDrawerState, useSceneGroupState } from "@/app/(typing)/type/_lib/atoms/stateAtoms";
+import {
+  useLineResultDrawerState,
+  useSceneGroupState,
+  useSetLineResultDrawer,
+} from "@/app/(typing)/type/_lib/atoms/stateAtoms";
 import { Sheet, SheetContent, SheetHeader, SheetTitle } from "@/components/ui/sheet";
 import ResultLineList from "./child/ResultLineList";
 
 function ResultDrawer() {
   const isOpen = useLineResultDrawerState();
   const sceneGroup = useSceneGroupState();
+  const setLineResultDrawer = useSetLineResultDrawer();
 
   return (
-    <Sheet modal={false} open={isOpen}>
+    <Sheet modal={false} open={isOpen} onOpenChange={setLineResultDrawer}>
       <SheetContent forceMount side="right" className="bg-accent/90 w-xs" overlayClassName="bg-transparent">
         <SheetHeader className="py-2">
           <SheetTitle>{sceneGroup === "End" ? "詳細リザルト" : "練習リザルト"}</SheetTitle>
