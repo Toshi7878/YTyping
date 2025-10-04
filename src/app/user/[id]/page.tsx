bd75cbe4f8954cae59ca6c71baa2a0f76683065a
Toshi7878 <toshiyuki19991125@gmail.com>
2025-10-04 11:07:29 +0900

    update

--- src/app/user/[id]/page.tsx
+++ src/app/user/[id]/page.tsx
@@ -1,21 +1,17 @@
 import { H1 } from "@/components/ui/typography";
-import { HydrateClient, prefetch, serverApi, trpc } from "@/trpc/server";
+import { serverApi } from "@/trpc/server";
 import { UserProfileCard } from "./_components/user-profile-card";
 import { UserTabs } from "./_components/user-tabs";
 
 export default async function Page({ params }: PageProps<"/user/[id]">) {
   const { id } = await params;
-
   const userProfile = await serverApi.userProfile.getUserProfile({ userId: Number(id) });
-  prefetch(trpc.userStats.getUserStats.queryOptions({ userId: Number(id) }));
 
   return (
-    <HydrateClient>
-      <div className="mx-auto max-w-screen-lg space-y-4">
-        <H1>プレイヤー情報</H1>
-        <UserProfileCard userProfile={userProfile} />
-        <UserTabs id={id} />
-      </div>
-    </HydrateClient>
+    <div className="mx-auto max-w-screen-lg space-y-4">
+      <H1>プレイヤー情報</H1>
+      <UserProfileCard userProfile={userProfile} />
+      <UserTabs id={id} />
+    </div>
   );
 }
