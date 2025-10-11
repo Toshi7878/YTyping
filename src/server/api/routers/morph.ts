779e460eb3e060b5018f8a802be597595c9336f0
Toshi7878 <toshiyuki19991125@gmail.com>
2025-10-12 00:12:34 +0900

    update

--- src/server/api/routers/morph.ts
+++ src/server/api/routers/morph.ts
@@ -10,9 +10,9 @@ export const morphConvertRouter = {
     return postAwsLambdaMorphApi(input.sentence);
   }),
 
-  getKanaWordYahoo: protectedProcedure.input(z.object({ sentence: z.string().min(1) })).query(async ({ input }) => {
-    return fetchYahooMorphAnalysis(input.sentence);
-  }),
+  // getKanaWordYahoo: protectedProcedure.input(z.object({ sentence: z.string().min(1) })).query(async ({ input }) => {
+  //   return fetchYahooMorphAnalysis(input.sentence);
+  // }),
 
   getCustomDict: protectedProcedure.query(async ({ ctx }) => {
     const dictionaryDict = await ctx.db
@@ -68,33 +68,33 @@ async function postAwsLambdaMorphApi(sentence: string): Promise<{ lyrics: string
   }
 }
 
-async function fetchYahooMorphAnalysis(sentence: string): Promise<string> {
-  const apiKey = env.YAHOO_APP_ID;
-  const apiUrl = "https://jlp.yahooapis.jp/MAService/V2/parse";
+// async function fetchYahooMorphAnalysis(sentence: string): Promise<string> {
+//   const apiKey = env.YAHOO_APP_ID;
+//   const apiUrl = "https://jlp.yahooapis.jp/MAService/V2/parse";
 
-  const response = await fetch(apiUrl, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "User-Agent": `Yahoo AppID: ${apiKey}`,
-    },
-    body: JSON.stringify({
-      id: apiKey,
-      jsonrpc: "2.0",
-      method: "jlp.maservice.parse",
-      params: {
-        q: sentence,
-        results: "ma",
-        response: {
-          surface: true,
-          reading: true,
-          pos: false,
-          baseform: false,
-        },
-      },
-    }),
-  });
+//   const response = await fetch(apiUrl, {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       "User-Agent": `Yahoo AppID: ${apiKey}`,
+//     },
+//     body: JSON.stringify({
+//       id: apiKey,
+//       jsonrpc: "2.0",
+//       method: "jlp.maservice.parse",
+//       params: {
+//         q: sentence,
+//         results: "ma",
+//         response: {
+//           surface: true,
+//           reading: true,
+//           pos: false,
+//           baseform: false,
+//         },
+//       },
+//     }),
+//   });
 
-  const data = await response.json();
-  return data.tokens.map((char: string) => char[1]).join("");
-}
+//   const data = await response.json();
+//   return data.tokens.map((char: string) => char[1]).join("");
+// }
