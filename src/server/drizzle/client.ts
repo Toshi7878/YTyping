0e8d947a84d8899f2d7c56bc0533e46029e19194
Toshi7878 <toshiyuki19991125@gmail.com>
2025-10-12 00:12:54 +0900

    update

--- src/server/drizzle/client.ts
+++ src/server/drizzle/client.ts
@@ -7,7 +7,7 @@ import * as schema from "./schema";
 
 const connectionString = env.DATABASE_URL;
 
-const client = postgres(connectionString);
+const client = postgres(connectionString, { prepare: false });
 
 export const db = drizzle(client, { schema });
 export type DBType = typeof db;
