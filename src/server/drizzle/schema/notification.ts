c913e658ecd6d311714547df81b7864acac738ea
Toshi7878 <toshiyuki19991125@gmail.com>
2025-10-13 16:36:11 +0900

    update

--- src/server/drizzle/schema/notification.ts
+++ src/server/drizzle/schema/notification.ts
@@ -15,29 +15,34 @@ export const Notifications = pgTable("notifications", {
   updatedAt: timestamp("updated_at").defaultNow().notNull(),
 });
 
-export const NotificationLikes = pgTable("notification_likes", {
-  notificationId: varchar("notification_id")
-    .primaryKey()
-    .references(() => Notifications.id, { onDelete: "cascade" }),
-  likerId: integer("liker_id")
-    .notNull()
-    .references(() => Users.id, { onDelete: "cascade" }),
-  mapId: integer("map_id")
-    .notNull()
-    .references(() => Maps.id, { onDelete: "cascade" }),
-});
+export const NotificationLikes = pgTable(
+  "notification_likes",
+  {
+    notificationId: varchar("notification_id")
+      .primaryKey()
+      .references(() => Notifications.id, { onDelete: "cascade" }),
+    likerId: integer("liker_id")
+      .notNull()
+      .references(() => Users.id, { onDelete: "cascade" }),
+    mapId: integer("map_id")
+      .notNull()
+      .references(() => Maps.id, { onDelete: "cascade" }),
+  },
 
-export const NotificationClaps = pgTable("notification_claps", {
-  notificationId: varchar("notification_id")
-    .primaryKey()
-    .references(() => Notifications.id, { onDelete: "cascade" }),
-  clapperId: integer("clapper_id")
-    .notNull()
-    .references(() => Users.id, { onDelete: "cascade" }),
-  resultId: integer("result_id")
-    .notNull()
-    .references(() => Results.id, { onDelete: "cascade" }),
-});
+  (t) => [uniqueIndex("uq_notification_likes_liker_id_map_id").on(t.likerId, t.mapId)],
+);
+
+export const NotificationClaps = pgTable(
+  "notification_claps",
+  {
+    notificationId: varchar("notification_id")
+      .primaryKey()
+      .references(() => Notifications.id, { onDelete: "cascade" }),
+    clapperId: integer("clapper_id").references(() => Users.id, { onDelete: "cascade" }),
+    resultId: integer("result_id").references(() => Results.id, { onDelete: "cascade" }),
+  },
+  (t) => [uniqueIndex("uq_notification_claps_clapper_id_result_id").on(t.clapperId, t.resultId)],
+);
 
 export const NotificationOverTakes = pgTable(
   "notification_over_takes",
@@ -67,6 +72,6 @@ export const NotificationOverTakes = pgTable(
     //   name: "notification_over_takes_visited_result_fk",
     // }).onDelete("cascade"),
 
-    uniqueIndex("uq_overtake_visitor_id_visited_id_map_id").on(t.visitorId, t.visitedId, t.mapId),
+    uniqueIndex("uq_notification_over_takes_visitor_id_visited_id_map_id").on(t.visitorId, t.visitedId, t.mapId),
   ],
 );
