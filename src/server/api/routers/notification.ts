6d394e2be301b7b114508fe0bf0d8f43f5e519fc
Toshi7878 <toshiyuki19991125@gmail.com>
2025-11-19 22:01:20 +0900

    update

--- src/server/api/routers/notification.ts
+++ src/server/api/routers/notification.ts
@@ -15,156 +15,154 @@ export const notificationRouter = {
 
     return isUnreadNotificationFound;
   }),
-  getInfinite: protectedProcedure
-    .input(z.object({ cursor: z.number().nullable().optional() }))
-    .query(async ({ input, ctx }) => {
-      const { db, user } = ctx;
-      const PAGE_SIZE = 20;
-      const { limit, offset, buildPageResult } = createPagination(input?.cursor, PAGE_SIZE);
+  getInfinite: protectedProcedure.input(z.object({ cursor: z.number().optional() })).query(async ({ input, ctx }) => {
+    const { db, user } = ctx;
+    const PAGE_SIZE = 20;
+    const { limit, offset, buildPageResult } = createPagination(input?.cursor, PAGE_SIZE);
 
-      const mapQuery = {
-        columns: {
-          creatorComment: false,
-          category: false,
-          createdAt: false,
-          creatorId: false,
-          playCount: false,
-          tags: false,
+    const mapQuery = {
+      columns: {
+        creatorComment: false,
+        category: false,
+        createdAt: false,
+        creatorId: false,
+        playCount: false,
+        tags: false,
+      },
+      with: {
+        creator: { columns: { id: true, name: true } },
+        difficulty: { columns: { romaKpmMedian: true, romaKpmMax: true } },
+        mapLikes: {
+          where: and(eq(MapLikes.userId, user.id)),
+          columns: { hasLiked: true },
+          limit: 1,
         },
-        with: {
-          creator: { columns: { id: true, name: true } },
-          difficulty: { columns: { romaKpmMedian: true, romaKpmMax: true } },
-          mapLikes: {
-            where: and(eq(MapLikes.userId, user.id)),
-            columns: { hasLiked: true },
-            limit: 1,
-          },
-          results: {
-            where: and(eq(Results.userId, user.id)),
-            columns: { rank: true, updatedAt: true },
-            limit: 1,
-          },
+        results: {
+          where: and(eq(Results.userId, user.id)),
+          columns: { rank: true, updatedAt: true },
+          limit: 1,
         },
-      } as const;
+      },
+    } as const;
 
-      const notifications = await db.query.Notifications.findMany({
-        columns: {
-          id: true,
-          type: true,
-          updatedAt: true,
+    const notifications = await db.query.Notifications.findMany({
+      columns: {
+        id: true,
+        type: true,
+        updatedAt: true,
+      },
+      with: {
+        overTake: {
+          with: {
+            visitor: { columns: { id: true, name: true } },
+            map: mapQuery,
+            visitedResult: { with: { status: { columns: { score: true } } } },
+            visitorResult: { with: { status: { columns: { score: true } } } },
+          },
         },
-        with: {
-          overTake: {
-            with: {
-              visitor: { columns: { id: true, name: true } },
-              map: mapQuery,
-              visitedResult: { with: { status: { columns: { score: true } } } },
-              visitorResult: { with: { status: { columns: { score: true } } } },
-            },
+        like: {
+          columns: {},
+          with: {
+            liker: { columns: { id: true, name: true } },
+            map: mapQuery,
           },
-          like: {
-            columns: {},
-            with: {
-              liker: { columns: { id: true, name: true } },
-              map: mapQuery,
-            },
-          },
-          clap: {
-            columns: {},
-            with: {
-              clapper: { columns: { id: true, name: true } },
-              result: {
-                with: {
-                  map: mapQuery,
-                  status: {
-                    columns: {
-                      minPlaySpeed: true,
-                    },
+        },
+        clap: {
+          columns: {},
+          with: {
+            clapper: { columns: { id: true, name: true } },
+            result: {
+              with: {
+                map: mapQuery,
+                status: {
+                  columns: {
+                    minPlaySpeed: true,
                   },
                 },
               },
             },
           },
         },
-        where: eq(Notifications.recipientId, user.id),
-        orderBy: desc(Notifications.updatedAt),
-        limit,
-        offset,
-      });
+      },
+      where: eq(Notifications.recipientId, user.id),
+      orderBy: desc(Notifications.updatedAt),
+      limit,
+      offset,
+    });
 
-      const toMapListItem = (map: (typeof notifications)[number]["overTake"]["map"], previewSpeed?: number) => {
-        return {
-          id: map.id,
-          updatedAt: map.updatedAt,
-          media: {
-            videoId: map.videoId,
-            previewTime: map.previewTime,
-            thumbnailQuality: map.thumbnailQuality,
-            previewSpeed,
-          },
-          info: { title: map.title, artistName: map.artistName, source: map.musicSource, duration: map.duration },
-          creator: { id: map.creator.id, name: map.creator.name },
-          difficulty: { romaKpmMedian: map.difficulty.romaKpmMedian, romaKpmMax: map.difficulty.romaKpmMax },
-          like: { count: map.likeCount, hasLiked: map.mapLikes?.[0]?.hasLiked ?? false },
-          ranking: {
-            count: map.rankingCount,
-            myRank: map.results?.[0]?.rank ?? null,
-            myRankUpdatedAt: map.results?.[0]?.updatedAt ?? null,
-          },
-        } satisfies MapListItem;
-      };
+    const toMapListItem = (map: (typeof notifications)[number]["overTake"]["map"], previewSpeed?: number) => {
+      return {
+        id: map.id,
+        updatedAt: map.updatedAt,
+        media: {
+          videoId: map.videoId,
+          previewTime: map.previewTime,
+          thumbnailQuality: map.thumbnailQuality,
+          previewSpeed,
+        },
+        info: { title: map.title, artistName: map.artistName, source: map.musicSource, duration: map.duration },
+        creator: { id: map.creator.id, name: map.creator.name },
+        difficulty: { romaKpmMedian: map.difficulty.romaKpmMedian, romaKpmMax: map.difficulty.romaKpmMax },
+        like: { count: map.likeCount, hasLiked: map.mapLikes?.[0]?.hasLiked ?? false },
+        ranking: {
+          count: map.rankingCount,
+          myRank: map.results?.[0]?.rank ?? null,
+          myRankUpdatedAt: map.results?.[0]?.updatedAt ?? null,
+        },
+      } satisfies MapListItem;
+    };
 
-      const items = notifications
-        .map((notification) => {
-          if (notification.type === "OVER_TAKE" && notification.overTake) {
-            const { overTake } = notification;
-            return {
-              id: notification.id,
-              type: notification.type,
-              updatedAt: notification.updatedAt,
-              visitor: {
-                id: overTake.visitorId,
-                name: overTake.visitor?.name ?? "名無し",
-                score: overTake.visitorResult.status.score,
-              },
-              myResult: {
-                prevRank: overTake.prevRank,
-                score: overTake.visitedResult.status.score,
-              },
-              map: toMapListItem(overTake.map),
-            };
-          }
+    const items = notifications
+      .map((notification) => {
+        if (notification.type === "OVER_TAKE" && notification.overTake) {
+          const { overTake } = notification;
+          return {
+            id: notification.id,
+            type: notification.type,
+            updatedAt: notification.updatedAt,
+            visitor: {
+              id: overTake.visitorId,
+              name: overTake.visitor?.name ?? "名無し",
+              score: overTake.visitorResult.status.score,
+            },
+            myResult: {
+              prevRank: overTake.prevRank,
+              score: overTake.visitedResult.status.score,
+            },
+            map: toMapListItem(overTake.map),
+          };
+        }
 
-          if (notification.type === "LIKE" && notification.like) {
-            const { like } = notification;
+        if (notification.type === "LIKE" && notification.like) {
+          const { like } = notification;
 
-            return {
-              id: notification.id,
-              type: notification.type,
-              updatedAt: notification.updatedAt,
-              liker: like.liker,
-              map: toMapListItem(like.map),
-            };
-          }
-          if (notification.type === "CLAP" && notification.clap) {
-            const { clap } = notification;
+          return {
+            id: notification.id,
+            type: notification.type,
+            updatedAt: notification.updatedAt,
+            liker: like.liker,
+            map: toMapListItem(like.map),
+          };
+        }
+        if (notification.type === "CLAP" && notification.clap) {
+          const { clap } = notification;
 
-            return {
-              id: notification.id,
-              type: notification.type,
-              updatedAt: notification.updatedAt,
-              clapper: clap.clapper,
-              map: toMapListItem(clap.result.map, clap.result.status.minPlaySpeed),
-            };
-          }
+          return {
+            id: notification.id,
+            type: notification.type,
+            updatedAt: notification.updatedAt,
+            clapper: clap.clapper,
+            map: toMapListItem(clap.result.map, clap.result.status.minPlaySpeed),
+          };
+        }
 
-          // フォールバック（通常は到達しない）
-          throw new Error(`Unknown notification action: ${notification.type}`);
-        })
-        .filter((item): item is NonNullable<typeof item> => item !== null);
+        // フォールバック（通常は到達しない）
+        throw new Error(`Unknown notification action: ${notification.type}`);
+      })
+      .filter((item): item is NonNullable<typeof item> => item !== null);
 
-      return buildPageResult(items);
-    }),
+    return buildPageResult(items);
+  }),
 
   postUserNotificationRead: protectedProcedure.mutation(async ({ ctx }) => {
     const { db, user } = ctx;
