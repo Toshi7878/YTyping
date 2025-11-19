e457061a801d144f187ed822d679e7eca566b3df
Toshi7878 <toshiyuki19991125@gmail.com>
2025-11-19 21:48:48 +0900

    update

--- src/utils/hooks/use-infinite-scroll.ts
+++ src/utils/hooks/use-infinite-scroll.ts
@@ -1,25 +1,16 @@
 import type { UseInfiniteQueryResult } from "@tanstack/react-query";
 import { useEffect } from "react";
-import { useInView } from "react-intersection-observer";
+import { type IntersectionOptions, useInView } from "react-intersection-observer";
 
-type InfiniteScrollProps<TData = unknown, TError = unknown> = Pick<
+type UseInfiniteScrollProps<TData = unknown, TError = unknown> = Pick<
   UseInfiniteQueryResult<TData, TError>,
   "isFetchingNextPage" | "fetchNextPage" | "hasNextPage"
 >;
 
-type InfiniteScrollOptions = {
-  rootMargin?: string;
-  threshold?: number;
-};
-
-export const useInfiniteScroll = <TData = unknown, TError = unknown>(
-  props: InfiniteScrollProps<TData, TError>,
-  options: InfiniteScrollOptions = {},
-) => {
+export const useInfiniteScroll = (props: UseInfiniteScrollProps, options: IntersectionOptions) => {
   const { isFetchingNextPage, fetchNextPage, hasNextPage } = props;
-  const { rootMargin = "1500px 0px", threshold = 0 } = options;
-
-  const { ref, inView } = useInView({ threshold, rootMargin });
+  const { rootMargin = "1500px 0px" } = options;
+  const { ref, inView } = useInView({ ...options, rootMargin });
 
   useEffect(() => {
     if (inView && hasNextPage && !isFetchingNextPage) {
