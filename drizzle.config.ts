dd9b785a1991380b88bf684901cd2210d0807437
Toshi7878 <toshiyuki19991125@gmail.com>
2025-11-29 17:31:15 +0900

    update

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,17 @@
 /** biome-ignore-all lint/style/noProcessEnv: <srcファイル外のためprocess.envを許容> */
 import { defineConfig } from "drizzle-kit";
 
+if (!process.env.DATABASE_URL) {
+  throw new Error("Missing POSTGRES_URL");
+}
+
+const nonPoolingUrl = process.env.DATABASE_URL.replace(":6543", ":5432");
+
 // biome-ignore lint/style/noDefaultExport: <export defaultする必要がある>
 export default defineConfig({
   schema: "./src/server/drizzle/schema.ts",
   out: "./drizzle",
   dialect: "postgresql",
-  dbCredentials: { url: process.env.DATABASE_URL ?? "" },
+  dbCredentials: { url: nonPoolingUrl },
   strict: true,
 });
