59e7d8290e9ba8cd6e95943fa07f96b60e950f04
Toshi7878 <toshiyuki19991125@gmail.com>
2025-12-14 12:31:16 +0900

    fix(header): update GitHub link to point to issue reporting

--- src/app/_components/header/lib/menu-items.ts
+++ src/app/_components/header/lib/menu-items.ts
@@ -4,7 +4,7 @@ type HeaderMenu = { title: string; href: Route; device?: "PC" };
 
 export const LEFT_MENU_LINK_ITEMS: HeaderMenu[] = [
   { title: "更新履歴", href: "/changelog" },
-  { title: "GitHub", href: "https://github.com/Toshi7878/YTyping" },
+  { title: "バグ報告 (GitHub)", href: "https://github.com/Toshi7878/YTyping/issues" },
   { title: "ツール", href: "/tools", device: "PC" },
   { title: "クレジット", href: "/credit" },
   { title: "プライバシーポリシー", href: "/privacy" },
