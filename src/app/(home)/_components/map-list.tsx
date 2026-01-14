e33ff529dd690724fc88b1cbc67891a1b7ae9747
Toshi7878 <toshiyuki19991125@gmail.com>
2026-01-14 22:05:57 +0900

    refactor(map-list): integrate sorting functionality by adding useMapListSortQueryState and updating query parameters for improved data retrieval

--- src/app/(home)/_components/map-list.tsx
+++ src/app/(home)/_components/map-list.tsx
@@ -3,22 +3,26 @@ import { useSuspenseInfiniteQuery } from "@tanstack/react-query";
 import { useEffect } from "react";
 import { InfiniteScrollSpinner } from "@/components/shared/infinite-scroll-spinner";
 import { MapCard } from "@/components/shared/map-card/card";
-import { useMapListFilterQueryStates } from "@/lib/search-params/map-list";
+import { useMapListFilterQueryStates, useMapListSortQueryState } from "@/lib/search-params/map-list";
 import { useTRPC } from "@/trpc/provider";
 import { setIsSearching, useIsSearchingState } from "../_lib/atoms";
 
 export const MapList = () => {
   const isSearching = useIsSearchingState();
   const trpc = useTRPC();
-  const [params] = useMapListFilterQueryStates();
+  const [filterParams] = useMapListFilterQueryStates();
+  const [sortParams] = useMapListSortQueryState();
 
   const { data, ...pagination } = useSuspenseInfiniteQuery(
-    trpc.mapList.get.infiniteQueryOptions(params, {
-      getNextPageParam: (lastPage) => lastPage.nextCursor ?? undefined,
-      refetchOnWindowFocus: false,
-      staleTime: Infinity,
-      gcTime: Infinity,
-    }),
+    trpc.mapList.get.infiniteQueryOptions(
+      { ...filterParams, sort: sortParams },
+      {
+        getNextPageParam: (lastPage) => lastPage.nextCursor ?? undefined,
+        refetchOnWindowFocus: false,
+        staleTime: Infinity,
+        gcTime: Infinity,
+      },
+    ),
   );
 
   useEffect(() => {
