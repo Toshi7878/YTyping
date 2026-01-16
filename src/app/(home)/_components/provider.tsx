954d0f925b7d77e76d18f7f2baff0aff9db863be
Toshi7878 <toshiyuki19991125@gmail.com>
2026-01-17 03:42:58 +0900

    chore: update .gitignore and refactor AtomsHydrator usage in JotaiProvider
    - Added .turbo to .gitignore to exclude Turborepo files.
    - Simplified AtomsHydrator component usage in JotaiProvider by removing unnecessary props.

--- src/app/(home)/_components/provider.tsx
+++ src/app/(home)/_components/provider.tsx
@@ -14,10 +14,7 @@ export const JotaiProvider = ({ children, userOptions }: JotaiProviderProps) => 
   const store = getHomeAtomStore();
   return (
     <Provider store={store}>
-      <AtomsHydrator
-        atomValues={[[listLayoutTypeAtom, userOptions?.mapListLayout ?? "TWO_COLUMNS"]]}
-        dangerouslyForceHydrate
-      >
+      <AtomsHydrator atomValues={[[listLayoutTypeAtom, userOptions?.mapListLayout ?? "TWO_COLUMNS"]]}>
         {children}
       </AtomsHydrator>
     </Provider>
