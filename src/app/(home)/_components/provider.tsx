8af05725dbd2c066749c7f50a24bf82393842ecc
Toshi7878 <toshiyuki19991125@gmail.com>
2026-01-18 23:58:00 +0900

    refactor(provider): simplify AtomsHydrator usage in JotaiProvider component
    - Removed the dangerouslyForceHydrate prop from the AtomsHydrator in the JotaiProvider component to streamline its implementation.
    - This change enhances code clarity while maintaining the intended functionality of the component.

--- src/app/(home)/_components/provider.tsx
+++ src/app/(home)/_components/provider.tsx
@@ -17,10 +17,7 @@ export const JotaiProvider = ({ children, userOptions }: JotaiProviderProps) => 
   // }, [userOptions]);
   return (
     <Provider store={store}>
-      <AtomsHydrator
-        atomValues={[[listLayoutTypeAtom, userOptions?.mapListLayout ?? "TWO_COLUMNS"]]}
-        dangerouslyForceHydrate
-      >
+      <AtomsHydrator atomValues={[[listLayoutTypeAtom, userOptions?.mapListLayout ?? "TWO_COLUMNS"]]}>
         {children}
       </AtomsHydrator>
     </Provider>
