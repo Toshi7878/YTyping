a3a5f45dc80a2e1d0093d5c28d3f5fa0071f5b4c
Toshi7878 <toshiyuki19991125@gmail.com>
2026-02-13 22:00:34 +0900

    Revert "test"
    This reverts commit 41904e469cde8512560d6fc2e0febd5ba8127352.

--- src/app/(home)/_components/map-control-area.tsx
+++ src/app/(home)/_components/map-control-area.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { VolumeRange } from "@/components/shared/volume-range";
 import { usePreviewPlayerState } from "@/lib/atoms/global-atoms";
-import { getBaseURL } from "@/lib/get-base-url";
 import { MapFilter } from "./search/map-filter";
 import { SearchInput } from "./search/search-input";
 import { SortControlsAndMapCount } from "./search/sort-controls-and-map-count";
@@ -12,7 +11,6 @@ export const MapControlArea = () => {
   return (
     <section className="mb-4 flex w-full flex-col gap-4">
       <SearchInput />
-      {getBaseURL()}
       <div className="flex flex-col justify-between space-y-4 md:flex-row md:space-y-0">
         <MapFilter />
         <VolumeRange YTPlayer={YTPlayer} />
