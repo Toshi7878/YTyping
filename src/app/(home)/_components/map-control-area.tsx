450d8bcc80a3a27bc4652165321b3a57d6a4c60d
Toshi7878 <toshiyuki19991125@gmail.com>
2026-03-21 14:38:24 +0900

    feat(map-control): add maintenance notice to MapControlArea component for user awareness

--- src/app/(home)/_components/map-control-area.tsx
+++ src/app/(home)/_components/map-control-area.tsx
@@ -11,6 +11,7 @@ export const MapControlArea = () => {
   return (
     <section className="mb-4 flex w-full flex-col gap-4">
       <SearchInput />
+      お知らせ：本日19時からメンテナンスを行います。メンテナンス中は一時的に接続できなくなります。
       <div className="flex flex-col justify-between space-y-4 md:flex-row md:space-y-0">
         <MapFilter />
         <VolumeRange YTPlayer={YTPlayer} />
