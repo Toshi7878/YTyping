4e456d8ba77b47ea078c247a0037abcc8ac944b4
Toshi7878 <toshiyuki19991125@gmail.com>
2026-03-21 13:32:15 +0900

    Revert "refactor(proxy): comment out proxy function and related logic for future implementation"
    This reverts commit 0c0ecc9be334f599ca5c0606629a84d542f03068.

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,42 +1,46 @@
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+import { env } from "@/env";
+import { auth } from "@/lib/auth";
 
 export const config = {
   matcher: ["/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)"],
 };
 
-// const authRoutes = ["/user/register", "/user/settings"];
+const authRoutes = ["/user/register", "/user/settings"];
 
-// export async function proxy(request: NextRequest) {
-//   const { nextUrl } = request;
-//   const { pathname } = nextUrl;
+export async function proxy(request: NextRequest) {
+  const { nextUrl } = request;
+  const { pathname } = nextUrl;
 
-//   const session = await auth.api.getSession({ headers: request.headers });
-//   const isLoggedIn = !!session?.user;
+  const session = await auth.api.getSession({ headers: request.headers });
+  const isLoggedIn = !!session?.user;
 
-//   if (isLoggedIn) {
-//     const userName = session.user.name;
+  if (isLoggedIn) {
+    const userName = session.user.name;
 
-//     const isMaintenanceMode = env.NEXT_PUBLIC_MAINTENANCE_MODE !== "false";
-//     if (isMaintenanceMode) {
-//       if (pathname !== "/maintenance") {
-//         return NextResponse.redirect(new URL("/maintenance", nextUrl));
-//       }
-//     } else if (pathname === "/maintenance") {
-//       return NextResponse.redirect(new URL("/404", nextUrl));
-//     }
+    const isMaintenanceMode = env.NEXT_PUBLIC_MAINTENANCE_MODE !== "false";
+    if (isMaintenanceMode) {
+      if (pathname !== "/maintenance") {
+        return NextResponse.redirect(new URL("/maintenance", nextUrl));
+      }
+    } else if (pathname === "/maintenance") {
+      return NextResponse.redirect(new URL("/404", nextUrl));
+    }
 
-//     if (userName) {
-//       if (pathname === "/user/register") {
-//         return NextResponse.redirect(new URL("/", nextUrl));
-//       }
-//     } else if (pathname !== "/user/register") {
-//       return NextResponse.redirect(new URL("/user/register", nextUrl));
-//     }
-//   } else {
-//     const isAuthRoute = authRoutes.includes(pathname);
-//     if (isAuthRoute) {
-//       return NextResponse.redirect(new URL("/", nextUrl));
-//     }
-//   }
+    if (userName) {
+      if (pathname === "/user/register") {
+        return NextResponse.redirect(new URL("/", nextUrl));
+      }
+    } else if (pathname !== "/user/register") {
+      return NextResponse.redirect(new URL("/user/register", nextUrl));
+    }
+  } else {
+    const isAuthRoute = authRoutes.includes(pathname);
+    if (isAuthRoute) {
+      return NextResponse.redirect(new URL("/", nextUrl));
+    }
+  }
 
-//   return NextResponse.next();
-// }
+  return NextResponse.next();
+}
