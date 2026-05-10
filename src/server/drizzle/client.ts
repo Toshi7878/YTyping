fe4e71e19750d122358982baa31b8715597b2eca
Toshi7878 <toshiyuki19991125@gmail.com>
2026-05-10 20:39:30 +0900

    refactor: integrate postgres client into drizzle setup for enhanced database interaction
    - Added postgres client to the drizzle initialization, allowing for improved database connection handling.
    - Updated the export of the db instance to utilize the new client configuration, enhancing overall database interaction capabilities.

--- src/server/drizzle/client.ts
+++ src/server/drizzle/client.ts
@@ -1,7 +1,10 @@
 import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
 import { env } from "@/env";
 import { relations } from "./relations";
 
-export const db = drizzle(env.DATABASE_URL, { relations });
+const client = postgres(env.DATABASE_URL, { prepare: false });
+
+export const db = drizzle({ relations, client });
 export type DBType = typeof db;
 export type TXType = Parameters<Parameters<DBType["transaction"]>[0]>[0];
